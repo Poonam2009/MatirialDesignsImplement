18252c072081a5274c7c1ec41abfbc16194ca6c2
Poonam2009 <35101072+Poonam2009@users.noreply.github.com>
2018-12-19 01:23:27 +0530

    addSoreData18Dec2018

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import {Grid} from '@material-ui/core'
+import LeftPane from './LeftPane'
+import RightPane from './RightPane'
+const styles = {
+  Paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10
+  }
+}
 
 export default props => 
-<div>
-  Hello From the Exercises
-</div>
+  <Grid container spacing={24}>
+    <Grid item xs={6}>
+      <LeftPane styles ={styles} />
+    </Grid>
+
+    <Grid item xs={6}>
+      <RightPane styles={styles} />
+    </Grid>
+</Grid>
---
+++ src/store.js
@@ -0,0 +1,55 @@
+export const muscles = [
+  'shoulders', 'chest', 'arms', 'back', 'legs'
+]
+
+export const exercises = [
+  {
+    id: 'overhead-press',
+    titles: 'Overhead Press',
+    discriptions: 'Delts  ',
+    muscles: 'shoulders'
+  },
+
+  {
+    id: 'dips',
+    titles: 'Dips',
+    discriptions: 'Triceps exercise...',
+    muscles: 'arms'
+  },
+
+  {
+    id: 'barbell-curls',
+    titles: 'Barbell Curls',
+    discriptions: 'Triceps exercise...',
+    muscles: 'arms'
+  },
+
+  {
+    id: 'bench-press',
+    titles: 'Bench Press',
+    discriptions: 'Chest exercise...',
+    muscles: 'chest'
+  },
+
+  {
+    id: 'pull-ups',
+    titles: 'Pull Ups',
+    discriptions: 'Back and biceps exercise...',
+    muscles: 'back'
+  },
+
+  {
+    id: 'deadlifts',
+    titles: 'Deadlifts',
+    discriptions: 'Back and legs exercises...',
+    muscles: 'back'
+  },
+
+  {
+    id: 'squats',
+    titles: 'Squats',
+    discriptions: 'legs exercises...',
+    muscles: 'legs'
+  }
+
+]
